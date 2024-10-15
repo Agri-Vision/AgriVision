64138782412194ad78bd06c213d6b7270f1be86a
IT21036620 <it21036620@my.sliit.lk>
2024-10-16 00:29:09 +0530

    fix: routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,10 +7,7 @@ const IndexRoutes = () => {
   return (
     <div>
       <Navbar />
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <Home />
       <Footer />
     </div>
   )
